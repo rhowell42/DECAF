407d2cb79b3dc2e8711fd34a252320cf011a8f02
Ryan Howell <rhowell@icarusgpvm02.fnal.gov>
2022-09-14 14:34:16 -0500

    load spectra per event number, get total events in the caf as the upper limit

--- event_display.C
+++ event_display.C
@@ -3,9 +3,9 @@
 
 #include "sbnana/CAFAna/Core/Binning.h"
 
-#include "display_vars.h"
-#include "data_funcs.h"
-#include "GUI.h"
+#include "guitools/display_vars.h"
+#include "tools/data_funcs.h"
+#include "guitools/GUI.h"
 
 #include "TFile.h"
 #include "TH2.h"
@@ -23,23 +23,22 @@ TH3F *frame3d = new TH3F(title.c_str(),title.c_str(),1,-400,400,1,-1000,1000,1,-
 bool ColorByPFPs = false;
 bool ColorBySlice = true;
 
-std::vector<std::vector<double>> X;
-std::vector<std::vector<double>> Y;
-std::vector<std::vector<double>> Z;
+std::vector<double> X;
+std::vector<double> Y;
+std::vector<double> Z;
 
-std::vector<std::vector<double>> PFPID;
-std::vector<std::vector<double>> SliceID;
+std::vector<double> PFPID;
+std::vector<double> SliceID;
 
-int spill = 0; 
-int nSpills = 0;
+unsigned int spill = 0; 
+unsigned int nSpills = 10;
 
 const SpillVar kFindEvents([](const caf::SRSpillProxy* sr) -> int {
-  X.push_back(kTPCX(sr));
-  Y.push_back(kTPCY(sr));
-  Z.push_back(kTPCZ(sr));
-  PFPID.push_back(kPFPID(sr));
-  SliceID.push_back(kSliceID(sr));
-  nSpills++;
+  X = kTPCX(sr);
+  Y = kTPCY(sr);
+  Z = kTPCZ(sr);
+  PFPID = kPFPID(sr);
+  SliceID = kSliceID(sr);
   return 42;
 });
 
@@ -54,21 +53,21 @@ void Draw()
   }
 
   if (ColorBySlice) {
-    std::vector<double> uniqueSliceIDs = SliceID[spill];
+    std::vector<double> uniqueSliceIDs = SliceID;
     std::vector<double>::iterator it;
     it = std::unique(uniqueSliceIDs.begin(),uniqueSliceIDs.end());
     uniqueSliceIDs.resize(std::distance(uniqueSliceIDs.begin(),it));
 
     int c = 0;
     for (int sliceID : uniqueSliceIDs) {
-      TPolyMarker3D *plot = new TPolyMarker3D(X[spill].size());
+      TPolyMarker3D *plot = new TPolyMarker3D(X.size());
       if (c == 10 || c == 19) { c++; }
 
       int target = sliceID; 
-      std::vector<int> indices = findItems(SliceID[spill], target);
+      std::vector<int> indices = findItems(SliceID, target);
       for (auto &e: indices) {
-        if (SliceID[spill][e] != sliceID) { continue; } 
-        plot->SetPoint(e,X[spill][e],Z[spill][e],Y[spill][e]);
+        if (SliceID[e] != sliceID) { continue; } 
+        plot->SetPoint(e,X[e],Z[e],Y[e]);
       }
       plot->SetMarkerSize(.2);
       plot->SetMarkerColor(1+c);
@@ -78,7 +77,7 @@ void Draw()
     }
   }
   else if (ColorByPFPs) {
-    std::vector<double> uniquePFPs = PFPID[spill];
+    std::vector<double> uniquePFPs = PFPID;
     std::vector<double>::iterator it;
     std::sort(uniquePFPs.begin(), uniquePFPs.end());
     it = std::unique(uniquePFPs.begin(),uniquePFPs.end());
@@ -86,14 +85,14 @@ void Draw()
    
     int c = 0;
     for (int pfp : uniquePFPs) {
-      TPolyMarker3D *plot = new TPolyMarker3D(X[spill].size());
+      TPolyMarker3D *plot = new TPolyMarker3D(X.size());
       if (c == 10 || c == 19) { c++; }
 
       int target = pfp; 
-      std::vector<int> indices = findItems(PFPID[spill], target);
+      std::vector<int> indices = findItems(PFPID, target);
       for (auto &e: indices) {
-        if (PFPID[spill][e] != pfp) { continue; } 
-        plot->SetPoint(e,X[spill][e],Z[spill][e],Y[spill][e]);
+        if (PFPID[e] != pfp) { continue; } 
+        plot->SetPoint(e,X[e],Z[e],Y[e]);
       }
       plot->SetMarkerSize(.2);
       plot->SetMarkerColor(2+c);
@@ -110,6 +109,7 @@ void Draw()
 void doAdvanceSpill()
 {
   if (spill < nSpills - 1) {
+    fill_vectors();
     spill++;
     Draw();
   }
@@ -117,6 +117,7 @@ void doAdvanceSpill()
 void doPreviousSpill()
 {
   if (spill > 0) {
+    fill_vectors();
     spill--;
     Draw();
   }
@@ -159,7 +160,6 @@ void fill_vectors()
   Z.clear();
   PFPID.clear();
   SliceID.clear();
-  nSpills = 0;
   SpectrumLoader loader(fname);
 
   const Binning bins = Binning::Simple(100, 0, 5000);
@@ -170,13 +170,15 @@ void fill_vectors()
   }
 
   Spectrum sFindSpill("",bins,loader,kFindEvents,thisCut,kSpillUnweighted);
-
-  loader.Go();
+  loader.Select(spill);
 }
 
 void event_display(const std::string inputName)
 {
   fname = inputName;
+  SpectrumLoader loader(fname);
+  nSpills = loader.Nevents();
+
   new MyMainFrame();
   frame3d->SetTitle("Event Display");
   frame3d->GetXaxis()->SetTitle("X position");
