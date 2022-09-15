5db4db7f40bc517942d1b6954d6aef7617dfab01
Ryan Howell <rhowell@icarusgpvm02.fnal.gov>
2022-09-15 08:46:55 -0500

    added a setup script for getting the right version of sbnana, and fhicl files for creating samples for the event display

---
+++ scripts/setup_online.sh
@@ -0,0 +1,6 @@
+source /cvmfs/icarus.opensciencegrid.org/products/icarus/setup_icarus.sh
+source /cvmfs/sbn.opensciencegrid.org/products/sbn/setup_sbn.sh
+
+setup sbnana -v v09_58_02 -q e20:prof
+#unsetup srproxy
+#setup srproxy v00.35 -qpy3
