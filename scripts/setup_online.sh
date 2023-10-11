d5c366414dd8a91f393fbeffcd07ac2b952b2d35
rhowell42 <39918054+rhowell42@users.noreply.github.com>
2023-10-11 09:17:50 -0400

    Update setup_online.sh

--- scripts/setup_online.sh
+++ scripts/setup_online.sh
@@ -1,6 +1,4 @@
 source /cvmfs/icarus.opensciencegrid.org/products/icarus/setup_icarus.sh
 source /cvmfs/sbn.opensciencegrid.org/products/sbn/setup_sbn.sh
 
-setup sbnana -v v09_69_01 -q e20:prof
-#unsetup srproxy
-#setup srproxy v00.35 -qpy3
+setup sbnana -v v09_75_03 -q e20:prof
